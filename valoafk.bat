8e3f9c2b14903e1c761ebcbcb3bfedf61f14d373
Arache Abderrahmane <46249965+Ar-mane@users.noreply.github.com>
2023-11-05 01:02:39 +0100

    Update valoafk.bat

--- valoafk.bat
+++ valoafk.bat
@@ -1,1 +1,18 @@
-python valoafk.py
+@echo off
+
+rem Define the path to the Python interpreter
+set python_executable=python
+
+rem Check if the required packages are installed
+%python_executable% -m pip show package_name > nul 2>&1
+if errorlevel 1 (
+    echo Installing required packages...
+    %python_executable% -m pip install -r requirements.txt
+) else (
+    echo Required packages are already installed.
+)
+
+rem Run the "valoafk.py" script
+%python_executable% valoafk.py
+
+pause
